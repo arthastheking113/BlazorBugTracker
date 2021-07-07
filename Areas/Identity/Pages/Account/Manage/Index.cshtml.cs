868f990539886911890d2b3752882e4996e0ce79
arthas <arthastheking113@gmail.com>
2021-07-06 23:14:25 -0400

    update manage user profile site

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorBugTracker.Data;
 using BlazorBugTracker.Models;
+using BlazorBugTracker.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
 {
@@ -14,13 +18,17 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<CustomUser> _userManager;
         private readonly SignInManager<CustomUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
 
         public IndexModel(
             UserManager<CustomUser> userManager,
-            SignInManager<CustomUser> signInManager)
+            SignInManager<CustomUser> signInManager, ApplicationDbContext context, IImageService imageService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
+            _imageService = imageService;
         }
 
         public string Username { get; set; }
@@ -36,6 +44,21 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Display(Name = "First Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Company")]
+            public int? CompanyId { get; set; }
+
+            [Display(Name = "Last Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string LastName { get; set; }
+
+            [Display(Name = "Change Avatar")]
+            public Byte[]? ImageData { get; set; }
+            public string ContentType { get; set; }
         }
 
         private async Task LoadAsync(CustomUser user)
@@ -47,7 +70,12 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                ImageData = user.ImageData,
+                ContentType = user.ContentType,
+                CompanyId = user.CompanyId
             };
         }
 
@@ -58,12 +86,12 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
+            ViewData["CompanyId"] = new SelectList(_context.Company, "Id", "Name");
             await LoadAsync(user);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(IFormFile image, Byte[]? imageData, string contentType)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -71,12 +99,34 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
+
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
                 return Page();
             }
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            user.CompanyId = (int)Input.CompanyId;
+            if (image != null)
+            {
+                user.ImageData = await _imageService.EncodeFileAsync(image);
+                user.ContentType = _imageService.RecordContentType(image);
+            }
+            else
+            {
+                if (imageData != null && contentType != null)
+                {
+                    user.ImageData = imageData;
+                    user.ContentType = contentType;
+                }
+                else
+                {
+                    user.ImageData = null;
+                    user.ContentType = null;
+                }
 
+            }
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
             {
@@ -87,7 +137,7 @@ namespace BlazorBugTracker.Areas.Identity.Pages.Account.Manage
                     return RedirectToPage();
                 }
             }
-
+            await _context.SaveChangesAsync();
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
