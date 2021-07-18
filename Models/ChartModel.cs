e97d8e280e5b97eb399f8995ab6558a5ba8a2890
arthas <arthastheking113@gmail.com>
2021-07-17 23:59:43 -0400

    upadte home page

---
+++ Models/ChartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBugTracker.Models
+{
+    public class ChartModel
+    {
+        public string TicketCategory { get; set; }
+        public double NumberOfTicket { get; set; }
+    }
+}
