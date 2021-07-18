e820a2faa892293ef0c331848a8e682662232a65
arthas <arthastheking113@gmail.com>
2021-07-18 02:12:29 -0400

    update notification page and delete search input in project ticket company notification

---
+++ Models/NotificationListModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBugTracker.Models
+{
+    public class NotificationListModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsViewed { get; set; }
+        public DateTime Created { get; set; }
+
+        public string SenderId { get; set; }
+        public virtual CustomUser Sender { get; set; }
+
+        public string RecipientId { get; set; }
+        public virtual CustomUser Recipient { get; set; }
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
+        public bool IsWelcome { get; set; }
+
+    }
+}
